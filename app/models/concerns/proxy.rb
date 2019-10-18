aa51eeedd10a38fda63fddd925c9baf79411f07c
Andy Braga <ab@luckypike.com>
2019-10-18 16:54:27 +0300

    Бакет для продакшна

--- app/models/concerns/proxy.rb
+++ app/models/concerns/proxy.rb
@@ -5,7 +5,8 @@ module Proxy
   IMG_SALT = [Rails.application.credentials.dig(:imgproxy, :salt)].pack('H*')
 
   def proxy_image_url(key, mode, width, height)
-    url = "s3://luckypike-develop/#{send(key).key}"
+    bucket = Rails.env.production? ? :production : :develop
+    url = "s3://luckypike-#{bucket}/#{send(key).key}"
     url = Base64.urlsafe_encode64(url).tr('=', '').scan(/.{1,16}/).join('/')
     url = "/#{mode}/#{width}/#{height}/sm/0/#{url}.jpg"
 
